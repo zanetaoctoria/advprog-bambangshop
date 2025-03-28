9e7a748aa928ecfb7e388c95ffc2fde74911e77e
zanetaoctoria <rebeccaocto@gmail.com>
2025-03-28 20:02:49 +0700

    Implement notify function in Notification service to notify each Subscriber.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -22,10 +22,29 @@ impl NotificationService{
         let product_type_str: &str = &product_type_upper.as_str();
         let result : Option<Subscriber> = SubscriberRepository::delete(product_type_str, url);
         if result.is_none(){
-         return Err (compose_error_response(
-             Status::NotFound,
-             String::from("Subscriber Not Found")));
+             return Err (compose_error_response(
+                 Status::NotFound,
+                 String::from("Subscriber Not Found")
+             ));
         }
         return Ok(result.unwrap());
+    }
+
+    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
+        let mut payload: Notification = Notification {
+            product_title : product.clone().title,
+            product_type: String::from(product_type),
+            product_url: product.clone().get_url(),
+            subscriber_name: String::from(""),
+            status: String::from(status)
+        };
+
+        let subscribers: Vec<Subscriber> = SubscriberRepository::list_all(product_type);
+        for subscriber in subscribers {
+            payload.subscriber_name = subscriber.clone().name;
+            let subscriber_clone = subscriber.clone();
+            let payload_clone = payload.clone();
+            thread::spawn(move|| subscriber_clone.update(payload_clone));
+        }
     }
 }
