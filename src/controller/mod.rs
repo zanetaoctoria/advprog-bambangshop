14a91612af191108557831c9c5cd67d06b3a96c1
zanetaoctoria <rebeccaocto@gmail.com>
2025-03-28 20:07:41 +0700

    Implement publish function in Program service and Program controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,3 +1,4 @@
+
 pub mod product;
 pub mod notification;
 
@@ -6,7 +7,8 @@ use rocket::fairing::AdHoc;
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
-            .mount("/product", routes![product::create, product::list, product::read, product::delete])
+            .mount("/product", routes![product::create, product::list,
+                product::read, product::delete, product::publish])
             .mount("/notification", routes![notification::subscribe, notification::unsubscribe])
     });
 }
--- src/service/product.rs
+++ src/service/product.rs
@@ -42,4 +42,18 @@ impl ProductService {
 
         return Ok(Json::from(product));
     }
+
+    pub fn publish(id: usize) -> Result<Product> {
+         let product_opt: Option<Product> = ProductRepository::get_by_id(id);
+         if product_opt.is_none() {
+             return Err(compose_error_response(
+                 Status::NotFound,
+                 String::from("Product not found.")
+             ));
+         }
+         let product: Product = product_opt.unwrap();
+
+         NotificationService.notify(&product.product_type, "PROMOTION", product.clone());
+         return Ok(product);
+    }
 }
